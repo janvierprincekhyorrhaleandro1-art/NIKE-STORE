bd59243a34f211215e82c01f70c8d24ddbbce9e9
janvierprincekhyorrhaleandro1-art <janvierprincekhyorrhaleandro1@gmail.com>
2026-09-23 12:09:18 -0400

    Remove image URL from defaultBanner
    Removed image URL from defaultBanner object.

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const defaultBanner = {
     title: "Nike Air Presto",
     subtitle: "Men's Shoes",
     productId: 1,
-    image: "https://i.ibb.co/2N4X33q/shoe-red.png"
+    image:
 };
 
 const defaultCategories = ["Sneakers / Soulye", "Rad / Vêtements", "Akseswa", "Lòt Bagay"];
